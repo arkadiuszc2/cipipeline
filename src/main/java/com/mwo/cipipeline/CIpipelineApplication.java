02c7f39eb069a000498a464987f08e60a38a619b
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2023-11-28 23:39:40 +0100

    first commit

---
+++ src/main/java/com/mwo/cipipeline/CIpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.mwo.cipipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CIpipelineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CIpipelineApplication.class, args);
+  }
+
+}
